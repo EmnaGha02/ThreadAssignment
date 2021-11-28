242ca3f52751c80dd47ce40d6f5b6ebd60c645c7
EmnaGha <ammounna01@gmail.com>
2021-11-28 19:07:34 +0100

    commit

--- src/ThreadAssignment.java
+++ src/ThreadAssignment.java
@@ -1,8 +1,13 @@
+import java.net.SocketPermission;
+
 public class ThreadAssignment {
 
     static class Counter {
         void count() {
-            //todo implement me
+            for (int i=350;i>0;i--){
+                System.out.println(i);
+            }
+            System.out.println("Finish !");
         }
     }
 
@@ -15,14 +20,29 @@ public class ThreadAssignment {
 
         @Override
         public void run() {
-            counter.count();
+            synchronized(counter) {
+                counter.count();
+            }
         }
     }
 
     public static void main(String[] args) {
         Counter counter = new Counter();
 
-        new MyThread(counter).start();
-        new MyThread(counter).start();
+        MyThread firstone =new MyThread(counter);
+        firstone.start();
+        
+        MyThread secondone =new MyThread(counter);
+        secondone.start();
+
+        try {
+            firstone.join();
+            secondone.join();
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("DONE!");
     }
+    
 }
